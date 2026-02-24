bca54a42b5205421422aa937e091d8dd14b1bf92
Ashwaq Ali <154123000+Azhwake@users.noreply.github.com>
2026-02-24 22:00:38 +0300

    ready for railway deploy

--- config/settings.py
+++ config/settings.py
@@ -90,9 +90,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+   'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
