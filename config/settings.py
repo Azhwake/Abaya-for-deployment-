9fed69aede340fb5d2e5bf3bc55a59f76473ff40
Ashwaq Ali <154123000+Azhwake@users.noreply.github.com>
2026-02-24 23:03:54 +0300

    ready for railway redeploy

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(
+    "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL")
     )
 }
