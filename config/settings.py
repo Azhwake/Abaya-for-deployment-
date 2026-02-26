02e64c26ed2a454d2b4254d8fb6529590a9c3fdd
Abdiwasac H Adnan <hasanabdiwasac@gmail.com>
2026-02-26 10:20:52 +0300

    Update ALLOWED_HOSTS to include additional production hostname

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["abaya-store-brod.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["abaya-store-brod.onrender.com", "127.0.0.1", "localhost", "web-production-a91d0.up.railway.app"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME') 
 if RENDER_EXTERNAL_HOSTNAME: 
